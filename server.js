fabb20d8b41483663920a4679a576948d72c4d36
zstem001hz-droid <259742879+zstem001hz-droid@users.noreply.github.com>
2026-05-18 18:27:26 -0400

    feat: add express app configuration and server

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 require("./db/connection");
 const bookRoutes = require("./routes/bookRoutes");
+
+// App Configuration
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+
+// Routes
+
+// Error Handling
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
